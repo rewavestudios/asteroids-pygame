c93533aa4bdd9e7ffe5e3323cb1f383b1a34d22b
rewavestudios <predrag.milanovic@apeiron-edu.eu>
2025-06-21 13:39:16 +0200

    F: Drawing the player

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame   # this allows us to use code from the open-source pygame library throughout this file
 from constants import *
+from player import Player
 
 def main():
     pygame.init()  # Initialize pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0  # Delta time (in seconds)
+
+    # Create player in the center of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         # Event handling - quit if window closed
@@ -14,6 +18,7 @@ def main():
                 return
 
         screen.fill((0, 0, 0))  # Fill screen with black
+        player.draw(screen) # Draw the player
         pygame.display.flip()   # Update the display
 
         # Limit to 60 FPS and get delta time
